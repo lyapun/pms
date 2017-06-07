24e59470ef289d089b58ddb8a6c6175bb63519cf
Taras Lyapun <taraslyapun@gmail.com>
2017-06-07 21:45:44 +0300

    Added ability to sign in

--- reservations/urls.py
+++ reservations/urls.py
@@ -8,4 +8,6 @@ router.register(r'reservations', ReservationViewSet)
 
 urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
