812c04c70894f75480f20c01474403bfb4ba6b27
Taras Lyapun <taraslyapun@gmail.com>
2017-06-07 21:40:57 +0300

    Added more tests for search

--- reservations/tests/api/test_get.py
+++ reservations/tests/api/test_get.py
@@ -51,6 +51,82 @@ class ReservationApiGetListTestCase(APITestCase):
         self.assertEqual(data['count'], 1)
         self.assertEqual(data['results'][0]['id'], current_res.id)
 
+    def test_should_search_for_exact_dates(self):
+        user = UserFactory()
+        self.client.login(username=user.username, password=TEST_PASSWORD)
+        reservation = ReservationFactory(
+            start_date=date.today() - timedelta(days=1),
+            end_date=date.today() + timedelta(days=1),
+        )
+        response = self.client.get(
+            '/api/v1/reservations/?start_date={}&end_date={}'.format(
+                date.today() - timedelta(days=1),
+                date.today() + timedelta(days=1)
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(len(data['results']), 1)
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(data['results'][0]['id'], reservation.id)
+
+    def test_should_search_if_start_date_overlap(self):
+        user = UserFactory()
+        self.client.login(username=user.username, password=TEST_PASSWORD)
+        reservation = ReservationFactory(
+            start_date=date.today(),
+            end_date=date.today() + timedelta(days=3),
+        )
+        response = self.client.get(
+            '/api/v1/reservations/?start_date={}&end_date={}'.format(
+                date.today() - timedelta(days=1),
+                date.today() + timedelta(days=1)
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(len(data['results']), 1)
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(data['results'][0]['id'], reservation.id)
+
+    def test_should_search_if_inside_start_end_range(self):
+        user = UserFactory()
+        self.client.login(username=user.username, password=TEST_PASSWORD)
+        reservation = ReservationFactory(
+            start_date=date.today() - timedelta(days=1),
+            end_date=date.today() + timedelta(days=3),
+        )
+        response = self.client.get(
+            '/api/v1/reservations/?start_date={}&end_date={}'.format(
+                date.today(),
+                date.today() + timedelta(days=1)
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(len(data['results']), 1)
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(data['results'][0]['id'], reservation.id)
+
+    def test_search_if_end_date_overlap(self):
+        user = UserFactory()
+        self.client.login(username=user.username, password=TEST_PASSWORD)
+        reservation = ReservationFactory(
+            start_date=date.today(),
+            end_date=date.today() + timedelta(days=3),
+        )
+        response = self.client.get(
+            '/api/v1/reservations/?start_date={}&end_date={}'.format(
+                date.today() + timedelta(days=1),
+                date.today() + timedelta(days=4),
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(len(data['results']), 1)
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(data['results'][0]['id'], reservation.id)
+
 
 class ReservationApiGetDetailsTestCase(APITestCase):
     def test_should_return_403_for_non_authenticated_user(self):
