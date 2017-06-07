b8f18792676b990379ae8880834957790a313499
Taras Lyapun <taraslyapun@gmail.com>
2017-06-07 15:25:34 +0300

    added tests for get details api

--- reservations/tests/api/test_get.py
+++ reservations/tests/api/test_get.py
@@ -15,7 +15,7 @@ class ReservationApiGetListTestCase(TestCase):
         self.client.login(username=user.username, password=TEST_PASSWORD)
 
         for _ in range(3):
-            ReservationFactory(creator=user)
+            ReservationFactory()
 
         response = self.client.get('/api/v1/reservations/')
         self.assertEqual(response.status_code, 200)
@@ -28,17 +28,14 @@ class ReservationApiGetListTestCase(TestCase):
         self.client.login(username=user.username, password=TEST_PASSWORD)
 
         ReservationFactory(
-            creator=user,
             start_date=date.today() - timedelta(days=5),
             end_date = date.today() - timedelta(days=2),
         )
         ReservationFactory(
-            creator=user,
             start_date=date.today() + timedelta(days=2),
             end_date = date.today() + timedelta(days=5),
         )
         current_res = ReservationFactory(
-            creator=user,
             start_date=date.today() - timedelta(days=1),
             end_date = date.today() + timedelta(days=1),
         )
@@ -53,3 +50,34 @@ class ReservationApiGetListTestCase(TestCase):
         self.assertEqual(len(data['results']), 1)
         self.assertEqual(data['count'], 1)
         self.assertEqual(data['results'][0]['id'], current_res.id)
+
+
+class ReservationApiGetDetailsTestCase(TestCase):
+    def test_should_return_403_for_non_authenticated_user(self):
+        response = self.client.get('/api/v1/reservations/1/')
+        self.assertEqual(response.status_code, 403)
+
+    def test_should_return_404_for_non_existed_object(self):
+        user = UserFactory()
+        self.client.login(username=user.username, password=TEST_PASSWORD)
+        response = self.client.get('/api/v1/reservations/777/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_should_return_403_for_auth_user_if_not_creator(self):
+        user = UserFactory()
+        reservation = ReservationFactory()
+        self.client.login(username=user.username, password=TEST_PASSWORD)
+        response = self.client.get('/api/v1/reservations/{}/'.format(
+            reservation.id
+        ))
+        self.assertEqual(response.status_code, 403)
+
+    def test_should_return_200_for_auth_user_if_creator(self):
+        user = UserFactory()
+        reservation = ReservationFactory(creator=user)
+        self.client.login(username=user.username, password=TEST_PASSWORD)
+        response = self.client.get('/api/v1/reservations/{}/'.format(
+            reservation.id
+        ))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['id'], reservation.id)
